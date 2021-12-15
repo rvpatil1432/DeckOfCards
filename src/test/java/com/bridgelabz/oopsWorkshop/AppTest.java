c49efdcd6d93a08fd87d1df0c18a79d625c7826c
rvpatil1432 <rvpatil1432@gmail.com>
2021-12-15 08:11:06 +0530

    Adding : Initial commit

---
+++ src/test/java/com/bridgelabz/oopsWorkshop/AppTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.oopsWorkshop;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
